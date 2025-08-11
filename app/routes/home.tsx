2a4565ade0b9f7c59688bf7d665b280f66f26410
uwuxverticuse <sagargusain150@gmail.com>
2025-08-11 19:07:31 +0530

    Created the Heading

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+      <section className= "main-section">
+          <div className= "page-heading">
+              <h1>Track Your Applications And Resume Rating</h1>
+              <h2> AI-driven feedback to perfect your applications and stand out to recruiters.</h2>/
+
+          </div>
+      </section>
+
+  </main>
 }
