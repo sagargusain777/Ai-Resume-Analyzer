337506c4ec5e02863c1218bf58beb2bf1b452f97
uwuxverticuse <sagargusain150@gmail.com>
2025-08-13 19:43:43 +0530

    Added Resume Card ,Feedback Circle

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import {Link} from "react-router";
-const Navbar:() => Element = () =>{
+const Navbar = () =>{
     return (
         < nav className="navbar">
             <Link to = "/">
                 <p className = "text-2xl font-bold text-gradient">HIREWISE</p>
             </Link>
             <Link to ="/upload" className="primary-button w-fit">
-                Upload Resume Hire Faster Drag me Faster
+                Upload Resume 
 
 
             </Link>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-
+import { resumes } from "../constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,16 +11,28 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  return (
 
+    //Main Container
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
-      <section className= "main-section">
-          <div className= "page-heading">
-              <h1>Track Your Applications And Resume Rating</h1>
-              <h2> AI-driven feedback to perfect your applications and stand out to recruiters.</h2>
 
-          </div>
-      </section>
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
 
-  </main>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </section>
+
+      
+    </main>
+  );
 }
